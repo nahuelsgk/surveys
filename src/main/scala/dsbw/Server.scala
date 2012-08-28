698ee6f3fed4b74114eba19d05551a983c5df014
Jordi Pradel <jordi.pradel@agilogy.com>
2012-08-28 14:05:04 +0200

    Initial static content added

--- src/main/scala/dsbw/Server.scala
+++ src/main/scala/dsbw/Server.scala
@@ -2,27 +2,37 @@ package dsbw
 
 import scala.util.Properties
 
-import org.eclipse.jetty.server.{Server => JettyServer};
-import org.eclipse.jetty.servlet.{ServletContextHandler, ServletHolder};
+import org.eclipse.jetty.server.{Server => JettyServer}
+import org.eclipse.jetty.servlet.{ServletContextHandler, ServletHolder}
 import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
+import org.eclipse.jetty.server.handler.{HandlerList, ResourceHandler}
 
 class Servlet extends HttpServlet {
+
   override def service(request: HttpServletRequest, response: HttpServletResponse) {
         response.setContentType("text/plain")
         response.setStatus(200)
-        response.getWriter.print("Hello, world!")
+        response.getWriter.print("Hello, world!!")
   }
+
 }
 
 object Server extends App {
   val port = Properties.envOrElse("PORT", "8080").toInt
   val server: JettyServer = new JettyServer(port)
 
-  val context = new ServletContextHandler(ServletContextHandler.NO_SESSIONS);
-  context.setContextPath("/");
-  context.addServlet(new ServletHolder(new Servlet()), "/");
+  val context = new ServletContextHandler(ServletContextHandler.NO_SESSIONS)
+  context.setContextPath("/")
+  context.addServlet(new ServletHolder(new Servlet()), "/")
 
-  server.setHandler(context);
+  val resourceHandler = new ResourceHandler
+  resourceHandler.setDirectoriesListed(true)
+  resourceHandler.setWelcomeFiles(Array("index.html"))
+  resourceHandler.setResourceBase("src/main/docroot")
 
-  server.start();
+  val handlers = new HandlerList()
+  handlers.setHandlers(Array(resourceHandler,context))
+
+  server.setHandler(handlers)
+  server.start()
 }
