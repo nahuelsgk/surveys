f2857a4ce21e2b4865b6fab9777765521cb4add7
Jordi Pradel <jordi.pradel@agilogy.com>
2012-08-29 14:24:05 +0200

    Ajax error handling added

--- src/main/docroot/scripts/chirper.js
+++ src/main/docroot/scripts/chirper.js
@@ -8,9 +8,23 @@ function addChirp(chirp){
 }
 
 $(function(){
-    $.getJSON("/api/chirps", function(tweets,textStatus,jqXHR){
-        $(tweets).each(function(){
-                addChirp(this);
-        });
-    })
+    var url = "/api/chirps";
+    $.ajax({
+        url: url,
+        success: function(tweets,textStatus,jqXHR){
+            $(tweets).each(function(){
+                    addChirp(this);
+            });
+        },
+        dataType: "json",
+        error: function(jqXHR, textStatus, error){
+            if(jqXHR && jqXHR.status==404){
+                $("<p>").addClass("error").text("Error getting the chirps... api server not found at " + url).appendTo("header")
+            } else {
+                $("<p>").addClass("error").text("Unknown error getting the chirps...").appendTo("header")
+            }
+            console.debug("Error!",jqXHR)
+            console.debug("Text status", textStatus)
+            console.debug("Error",error)
+        }})
 });
