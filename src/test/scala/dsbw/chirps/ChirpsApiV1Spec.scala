31ef095ecb6421745f1d94eb61627dcb5f762c59
Alex Carol <alex.carol.c@gmail.com>
2013-02-26 19:26:11 +0100

    Fixed test

--- src/test/scala/dsbw/chirps/ChirpsApiV1Spec.scala
+++ src/test/scala/dsbw/chirps/ChirpsApiV1Spec.scala
@@ -9,6 +9,7 @@ import org.bson.types.ObjectId
 import org.mockito.Mockito._
 import org.scalatest.Assertions._
 import dsbw.util.Mocks
+import dsbw.server.HttpStatusCode
 
 class ChirpsApiV1Spec extends FlatSpec with Mocks with BeforeAndAfterEach {
 
@@ -16,14 +17,15 @@ class ChirpsApiV1Spec extends FlatSpec with Mocks with BeforeAndAfterEach {
 
     val chirpsRepository = mock[ChirpsRepository]
     val chirpersRepository = mock[ChirpersRepository]
-    val api = new ChirpsApi(chirpsRepository,chirpersRepository)
+    val api = new ChirpsApi(new ChirpsService(chirpsRepository,chirpersRepository))
   }
 
   it should "return an empty list when listing all chirps and there are no chirps in the DB" in {
     new TestChirpsApi {
       when(chirpsRepository.findAll).thenReturn(List())
-      val chirps = api.listChirps
-      assert(chirps === List())
+      val response = api.service("GET", "/api/chirps")
+      assert(HttpStatusCode.Ok === response.status)
+      assert(Some(List()) === response.body)
     }
   }
 }
