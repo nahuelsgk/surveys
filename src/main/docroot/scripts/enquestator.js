1d0a7ed17b98395e3a5d52e35ca8ada543733e68
Alex Carol <alex.carol.c@gmail.com>
2013-03-14 17:57:51 +0100

    Creating new enquestator.js and index.html from the old ones

---
+++ src/main/docroot/scripts/enquestator.js
@@ -0,0 +1,70 @@
+(function(){
+
+var loggedUser = {
+    name: "John Doe",
+    username: "john_doe",
+    avatar: "/img/avatar1.png",
+    status: "The average John Doe, whose status is really, really boring."
+}
+
+var chirps = []
+
+function renderChirps(){
+
+    function addChirp(chirp){
+        var chirpElement = $("#chirpTemplate.template").clone().appendTo("#chirps").removeClass("template");
+        $("span.name",chirpElement).text(chirp.author.name);
+        $("span.username",chirpElement).text("@" + chirp.author.username);
+        $("p",chirpElement).text(chirp.message);
+        $("date",chirpElement).text(chirp.date);
+        $("img",chirpElement).attr("src",chirp.author.avatar);
+    }
+
+    $(chirps).each(function(){
+            addChirp(this);
+    });
+}
+
+function renderLoggedUser(){
+    $("header h1").text(loggedUser.name);
+    $("header h2").text("@" + loggedUser.username);
+    $("header p.status").text(loggedUser.status);
+    $("header img").attr("src",loggedUser.avatar)
+}
+
+$(function(){
+
+    renderLoggedUser();
+
+    var url = "/api/chirps";
+    $.ajax({
+        url: url,
+        success: function(newChirps,textStatus,jqXHR){
+            chirps = newChirps;
+            renderChirps();
+        },
+        dataType: "json",
+        error: function(jqXHR, textStatus, error){
+            if(jqXHR && jqXHR.status==404){
+                $("<p>").addClass("error").text("Error getting the chirps... api server not found at " + url).appendTo("header")
+            } else {
+                $("<p>").addClass("error").text("Unknown error getting the chirps...").appendTo("header")
+            }
+            console.debug("Error!",jqXHR)
+            console.debug("Text status", textStatus)
+            console.debug("Error",error)
+        }})
+});
+
+
+})();
+
+var t=setInterval(getNewChirps,5000);
+function getNewChirps(){
+  $('#msgid').empty();
+  $('#msgid').css("opacity","100");
+  $('#msgid').append("Searching new chirps...");
+  $('#msgid').animate({ opacity: 0 }, 1500);
+}
+
+
