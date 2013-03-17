858d2078656753811357937cdaedd499e6625cc5
Nahuel Velazco <nahuel@Rodio@fapac.org>
2013-03-17 10:43:00 +0100

    New surveys api skeleton

---
+++ src/main/scala/dsbw/surveys/DB.scala
@@ -0,0 +1,28 @@
+package dsbw.surveys
+
+import com.mongodb.casbah.MongoConnection
+import com.mongodb.casbah.commons.MongoDBObject
+
+/** A class representing the Mongo DB used. Each collection is a value of the class. */
+
+class DB(hostName: String, port: Int, dbName: String, username: String, pwd: String){
+  val db = MongoConnection(hostName, port)(dbName)
+  db.authenticate(username, pwd)
+  val surveys = db("surveys")
+
+  /** Initialize the DB here, mostly, ensure indexes */
+  def init() {
+    surveys.ensureIndex(MongoDBObject("date" -> -1))
+  }
+
+  /** In case you want to reset the DB. Warning! This will remove all the data! */
+  def reset() {
+    drop()
+    init()
+  }
+
+  /** Drop all the DB destroying all the data */
+  def drop() {
+    surveys.drop()
+  }
+}
---
+++ src/main/scala/dsbw/surveys/SurveysApi.scala
@@ -0,0 +1,29 @@
+package dsbw.surveys
+
+import dsbw.server.{Server, HttpStatusCode, Response, Api}
+import dsbw.json.JSON
+import Config.{dbHostName, dbPort, dbName, username, pwd, webServerPort}
+
+/** Surveyss API */
+class SurveysApi(surveysService:SurveysService) extends Api {
+
+  def service(method: String, uri: String, parameters: Map[String, List[String]] = Map(), headers: Map[String, String] = Map(), body: Option[JSON] = None): Response = {
+    println("--Called Service--");
+    (method + " " + uri) match {
+      case "POST /api/survey" => Response(HttpStatusCode.Ok, surveysService.listSurveys)
+      case _ => Response(HttpStatusCode.Ok, "Hello world!")
+    }
+  }
+
+}
+
+object SurveysApp extends App {
+
+  val db = new DB(dbHostName, dbPort, dbName, username, pwd)
+  val surveysRepository = new SurveysRepository(new SurveysDao(db))
+  val surveysService = new SurveysService(surveysRepository)
+
+  val server = new Server(new SurveysApi(surveysService), webServerPort)
+  server.start()
+
+}
