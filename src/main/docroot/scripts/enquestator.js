b8abcac099ccd788974a81e09f143743aa7bcbda
Alex Carol <alex.carol.c@gmail.com>
2013-03-17 11:17:34 +0100

    Added console log

--- src/main/docroot/scripts/enquestator.js
+++ src/main/docroot/scripts/enquestator.js
@@ -1,7 +1,7 @@
 (function(){
     $('#create_survey_form').ajaxForm(function() {
         //TODO check until >= since
-        alert("Thank you for your comment!");
+        console.log('post send form');
     });
 })();
 
