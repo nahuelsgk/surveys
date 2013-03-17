f6e018b9eb1638c79108793ba3eb4892d89ab947
Alex Carol <alex.carol.c@gmail.com>
2013-03-17 23:50:59 +0100

    Used document.ready and add automatical update of the date so until is never greater than since

--- src/main/docroot/scripts/enquestator.js
+++ src/main/docroot/scripts/enquestator.js
@@ -1,12 +1,21 @@
-(function(){
+$(document).ready(function($) {
+    formatNumber = function(number) {
+        return (number < 10) ?  '0' + number : number;
+    }
+    //setting since and until to the current day
+    d = new Date();
+    today = d.getFullYear() + '-' + formatNumber(d.getMonth()) + '-' + formatNumber(d.getDate() + 1);
+    $('#since').attr('value', today);
+    $('#until').attr('value', today);
+
+
     $('#create_survey_form').ajaxForm(function() {
-        //TODO check until >= since
-        if ($('#create_survey_form').attr('method') == 'PUT') {
+        if ($('#create_survey_form').attr('method') === 'PUT') {
             date = new Date();
             $('h2').text('Survey updated at ' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds   ());
         } else {
             $('#create_survey_form').attr('method', 'PUT');
-            $('#create_survey_form #submit_btn').attr('value','Edit');
+            $('#submit_btn').attr('value','Edit');
 
             $('h2').text('Survey sent');
             $('#survey_description').text('Click the edit button to update it');
@@ -14,5 +23,18 @@
         console.log('Sent Form with '+$('#create_survey_form').attr('method')+' Method');
 
     });
-})();
+
+    $('#since').change(function() {
+        if ($('#since').attr('value') > $('#until').attr('value')) {
+            $('#until').attr('value', $('#since').attr('value'));
+        }
+    });
+    $('#until').change(function() {
+        if ($('#since').attr('value') > $('#until').attr('value')) {
+                $('#since').attr('value', $('#until').attr('value'));
+        }
+    });
+
+});
+
 
