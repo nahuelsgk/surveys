516292b1b4c5e4e85ab2db6d33677c45d6f1b1b9
Alex Carol <alex.carol.c@gmail.com>
2013-03-17 21:45:44 +0100

    Customized form sending

--- src/main/docroot/scripts/enquestator.js
+++ src/main/docroot/scripts/enquestator.js
@@ -1,7 +1,18 @@
 (function(){
     $('#create_survey_form').ajaxForm(function() {
         //TODO check until >= since
-        console.log('post send form');
+        if ($('#create_survey_form').attr('method') == 'PUT') {
+            date = new Date();
+            $('h2').text('Survey updated at ' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds   ());
+        } else {
+            $('#create_survey_form').attr('method', 'PUT');
+            $('#create_survey_form #submit_btn').attr('value','Edit');
+
+            $('h2').text('Survey sent');
+            $('#survey_description').text('Click the edit button to update it');
+        }
+        console.log('Sent Form with '+$('#create_survey_form').attr('method')+' Method');
+
     });
 })();
 
