5c2d9aaf5ad2ab268b0cf3d0d9188e3a1da5fecd
Nahuel Velazco <nahuel@Rodio@fapac.org>
2013-03-25 09:08:34 +0100

    Regexp a la Api per la enrutacio de controladors

--- src/main/scala/dsbw/surveys/SurveysApi.scala
+++ src/main/scala/dsbw/surveys/SurveysApi.scala
@@ -8,8 +8,13 @@ import Config.{dbHostName, dbPort, dbName, username, pwd, webServerPort}
 class SurveysApi(surveysService:SurveysService) extends Api {
 
   def service(method: String, uri: String, parameters: Map[String, List[String]] = Map(), headers: Map[String, String] = Map(), body: Option[JSON] = None): Response = {
+
+    val patternGetSurveyId = "GET /api/survey/(\\d+)".r
+    
     (method + " " + uri) match {
       case "POST /api/survey" => Response(HttpStatusCode.Ok, "hi dude!", surveysService.createSurvey(body))
+      case patternGetSurveyId(id) => Response(HttpStatusCode.Ok, null, surveysService.getSurvey(id))
+      case "GET /api/surveys" => Response(HttpStatusCode.Ok, null, surveysService.listSurveys())
       case _ => Response(HttpStatusCode.Ok, null,"Hello world!")
     }
   }
--- src/main/scala/dsbw/surveys/SurveysService.scala
+++ src/main/scala/dsbw/surveys/SurveysService.scala
@@ -9,11 +9,16 @@ case class Question(question:String)
 class SurveysService(surveysRepository: SurveysRepository) {
 
   def listSurveys() {
-    println("Hay que devolver una lista de encuestas")
+    println("We must return a list of surveys")
   }
 
   def createSurvey(body: Option[JSON]){
     println("We must create a survey with this parameterers. We need to know how to deserialize it and catch the values")
     println(body.toString)
   }
+
+  def getSurvey(id: String){
+    println("We must return a survey "+id)
+  }
+
 }
