12246e6053e84f35d8c5c7e660a967a7b76371be
marcal_perapoch <marcal.perapoch@est.fib.upc.edu>
2013-04-09 15:52:39 +0200

    domain and views are in separed js now

--- src/main/docroot/scripts/domain.js
+++ src/main/docroot/scripts/domain.js
@@ -1,34 +1,49 @@
 /* Domain Objects */
-function Survey () {
+function Survey() {
     switch (arguments.length) {     //multiple constructors
-        case 3:
-            this.title = arguments[0];
-            this.since  = arguments[1];
-            this.until = arguments[2];
-            this.questions = new Array();
-        break;
         case 1:
             var json = arguments[0];
-            this.title = json.title;
-            this.since  = json.since;
-            this.until = json.until;
-            this.id = json.id;
-            this.questions = new Array();
+            init(this,json.id,json.title,json.since,json.until,json.state);
         break;
-        default : /*NOP*/
     }
 
+    this.addQuestion = function(question) {
+        questions[question.id] = question;
+    }
 
+    this.addAnswer = function(answer) {
+        answer[answer.id] = answer;
+    }
 
+    function init(survey, title, since, until, id, state) {
+        survey.title = title;
+        survey.since  = since;
+        survey.until = until;
+        survey.id = id;
+        survey.state = state;
+        survey.questions = new Object(); // Map for all the survey's questions
+        survey.answers = new Object(); // Map for all the survey's answers
+    }
 
 }
 
-function Question () {
+function Question() {
     switch (arguments.length) {
         case 3:
             this.id  = id;
             this.type  = type;
             this.order = order;
+        break;
+    }
+}
+
+function Answer() {
+    switch (arguments.length) {
+        case 3:
+            this.id  = id;
+            this.idQuestion  = type;
+            this.idClient = order;
+            this.idType = type;
         break;
     }
 }
